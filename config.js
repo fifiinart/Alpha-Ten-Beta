5e552f150661b7408d2f87aa99c6a3faba1da1db
fifiinart <ferenxpain64@163.com>
2019-04-26 12:24:27 -0400

    Add config file

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv')
+  .config();
+module.exports = {
+  botToken: process.env.BOT_TOKEN,
+  embedColor: 0x00FFFF,
+  version: '1.0.6',
+  prefix: 'a.',
+  owner: ['521817052470575105', '373317798430244864']
+}
